bf23e534b80eb4465d52fc3bfe682369db900a6d
anis-king-911 <91683396+anis-king-911@users.noreply.github.com>
2025-03-09 08:23:39 +0100

    Update server.js
    i checked the hono documentation, let's just hope it works.

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
-import { Hono } from "hono";
+// import { Hono } from "hono";
 
-const app = new Hono();
+// const app = new Hono();
+
+// app.get("/", (c) => {
+//   return c.json({ message: "Welcome to Hono!" });
+// });
+
+// app.get("/api/data", (c) => {
+//   return c.json({ data: [1, 2, 3, 4, 5] });
+// });
+
+// // import { serve } from "@hono/node-server";
+// // serve({ fetch: app.fetch, port: 7700 });
+// // console.log("🚀 Server running at http://localhost:7700");
+
+// export default app;
+
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+export const runtime = "edge";
+
+const app = new Hono(); //.basePath("/api")
 
 app.get("/", (c) => {
   return c.json({ message: "Welcome to Hono!" });
@@ -10,8 +30,4 @@ app.get("/api/data", (c) => {
   return c.json({ data: [1, 2, 3, 4, 5] });
 });
 
-// import { serve } from "@hono/node-server";
-// serve({ fetch: app.fetch, port: 7700 });
-// console.log("🚀 Server running at http://localhost:7700");
-
-export default app;
+export const GET = handle(app);
